fb3dd1f66495692c27f7ad63bcefb0f8caeac04c
Eddie Long <eddie.long@blippar.com>
2019-01-10 22:37:43 +0000

    Initial commit

---
+++ Proteus-Core/DegreesToRadians.swift
@@ -0,0 +1,18 @@
+//
+//  DegreesToRadians.swift
+//  Cork Weather
+//
+//  Created by Eddie Long on 26/09/2017.
+//  Copyright © 2017 eddielong. All rights reserved.
+//
+
+import Foundation
+
+public extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi / 180 }
+}
+
+public extension FloatingPoint {
+    var degreesToRadians: Self { return self * .pi / 180 }
+    var radiansToDegrees: Self { return self * 180 / .pi }
+}
---
+++ Proteus-Core/Proteus_Core.h
@@ -0,0 +1,19 @@
+//
+//  Proteus_Core.h
+//  Proteus-Core
+//
+//  Created by Eddie Long on 10/01/2019.
+//  Copyright © 2019 eddielong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Proteus_Core.
+FOUNDATION_EXPORT double Proteus_CoreVersionNumber;
+
+//! Project version string for Proteus_Core.
+FOUNDATION_EXPORT const unsigned char Proteus_CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Proteus_Core/PublicHeader.h>
+
+
